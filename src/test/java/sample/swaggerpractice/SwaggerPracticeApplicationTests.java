9e28c2109a9a0f3379f414c177627f923ac40881
chanhl22 <lch9502@naver.com>
2024-01-15 09:13:47 +0900

    init: 프로젝트 생성

---
+++ src/test/java/sample/swaggerpractice/SwaggerPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package sample.swaggerpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwaggerPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
